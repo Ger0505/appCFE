45faf7d8bb035f3e377d454dce4f81fabe36dbf4
Arciga <pacorrotearc@live.com>
2020-05-25 08:30:43 -0500

    AvanceFormtarea2

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/prueba", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/colaborador/colaborador.html"));
+  res.sendFile(path.join(__dirname, "../views/colaboradorr.html"));
 });
 
 module.exports = router;
