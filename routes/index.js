987a005c580b7bec802c68a234fe6da7101e1f45
Ger0505 <gerzonex360@gmail.com>
2020-05-25 18:58:16 -0500

    Rutas a las páginas html

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,24 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/prueba", function (req, res, next) {
+router.get("/tarea", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../views/tareas/agregarTarea.html"));
+});
+
+router.get("/colaborador", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/colaborador.html"));
+});
+
+router.get("/departamento", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/departamento.html"));
+});
+
+router.get("/puesto", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/puesto.html"));
+});
+
+router.get("/status", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/status.html"));
 });
 
 module.exports = router;
