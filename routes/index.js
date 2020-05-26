9c1309e632cf74b37aa462fce71caea30579ae3a
Ger0505 <gerzonex360@gmail.com>
2020-05-26 01:49:39 -0500

    Modificaciones y Comienzo CRUD

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,32 @@ router.get("/colaborador", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../views/colaborador/colaborador.html"));
 });
 
+router.get("/colaborador/actualizar", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/colaborador/actualizar_colaborador.html"));
+});
+
 router.get("/departamento", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../views/departamento/departamento.html"));
+});
+
+router.get("/departamento/actualizar", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/departamento/actualizar_departamento.html"));
 });
 
 router.get("/puesto", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../views/puesto/puesto.html"));
 });
 
+router.get("/puesto/actualizar", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/puesto/actualizar_puesto.html"));
+});
+
 router.get("/status", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../views/status/status.html"));
+});
+
+router.get("/status/actualizar", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/status/actualizar_status.html"));
 });
 
 module.exports = router;
