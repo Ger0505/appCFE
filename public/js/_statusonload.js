941a69fafa13614099fa831257f628787313bd87
Arciga <pacorrotearc@live.com>
2020-05-26 22:45:06 -0500

    Paso de parametros con HTML listo (mejor solucion)

---
+++ public/js/_statusonload.js
@@ -0,0 +1,25 @@
+const getParameters = (nombreParametro) => {
+	let sPageURL = window.location.href;
+	let sDeconstrucURL = sPageURL.split("/"),
+		sParameterName,
+		i;
+	for (let index = 0; index < sDeconstrucURL.length; index++) {
+		let sURLVariables = sDeconstrucURL[index].split("&");
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split("=");
+			if (sParameterName[0] === nombreParametro) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
+	}
+	return true;
+};
+$(document).ready(() => {
+	let id = getParameters("id");
+
+	// Aqui se inserta el código para meter los datos en el html
+	// por ejemplo;
+	// let h2 = $("#idStatus");
+	// h2.append();
+	// h2.innerHTML = `<h2>${id}</h2>`;
+});
--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,47 @@ const path = require("path");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+	res.render("index", { title: "Express" });
 });
 
 router.get("/tarea", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/tareas/agregarTarea.html"));
+	res.sendFile(path.join(__dirname, "../views/tareas/agregarTarea.html"));
 });
 
 router.get("/colaborador", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/colaborador/colaborador.html"));
+	res.sendFile(path.join(__dirname, "../views/colaborador/colaborador.html"));
 });
 
 router.get("/colaborador/actualizar", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/colaborador/actualizar_colaborador.html"));
+	res.sendFile(
+		path.join(__dirname, "../views/colaborador/actualizar_colaborador.html")
+	);
 });
 
 router.get("/departamento", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/departamento/departamento.html"));
+	res.sendFile(path.join(__dirname, "../views/departamento/departamento.html"));
 });
 
 router.get("/departamento/actualizar", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/departamento/actualizar_departamento.html"));
+	res.sendFile(
+		path.join(__dirname, "../views/departamento/actualizar_departamento.html")
+	);
 });
 
 router.get("/puesto", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/puesto/puesto.html"));
+	res.sendFile(path.join(__dirname, "../views/puesto/puesto.html"));
 });
 
 router.get("/puesto/actualizar", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/puesto/actualizar_puesto.html"));
+	res.sendFile(path.join(__dirname, "../views/puesto/actualizar_puesto.html"));
 });
 
 router.get("/status", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/status/status.html"));
+	res.sendFile(path.join(__dirname, "../views/status/status.html"));
 });
 
-router.get("/status/actualizar", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/status/actualizar_status.html"));
+router.get("/status/actualizar/:id", function (req, res, next) {
+	res.sendFile(path.join(__dirname, "../views/status/actualizar_status.html"));
 });
 
 module.exports = router;
